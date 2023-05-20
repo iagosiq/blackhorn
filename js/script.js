3da934296c87e8d296c9a9341b24c9bac7776f6a
iago siqueira <122619758+iagosiq@users.noreply.github.com>
2023-05-20 07:18:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,8 +7,12 @@
 
 // 
 
-if (options.style.display === 'none') {
-    options.style.display = 'block';
-} else {
-    options.style.display = 'none';
+function abrir_menu() {
+    var options = document.getElementById('menu-options');
+
+    if (options.style.display === 'none') {
+        options.style.display = 'block';
+    } else {
+        options.style.display = 'none';
+    }
 }
