5754102d33c33d769a3d25e0f758dd39dfbb275a
iago siqueira <122619758+iagosiq@users.noreply.github.com>
2023-05-20 06:43:42 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-var menuButton = document.getElementsByClassName('menu')
-var menuOptions = document.getElementById('menu-options')
+// var menuButton = document.getElementsByClassName('menu')
+// var menuOptions = document.getElementById('menu-options')
 
-menuButton.addEventListener('click', function() {
-    menuOptions.style.display = (menuOptions.style.display === 'block')
-})
+// menuButton.addEventListener('click', function() {
+//     menuOptions.style.display = (menuOptions.style.display === 'block')
+// })
+
+function abrir_menu(){
+    var options = document.getElementById('menu-options')
+    options.style.display = (options.style.display === 'block')
+}
